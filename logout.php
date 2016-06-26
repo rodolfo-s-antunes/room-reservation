188d453a1973927a3b6bd9d5d07cf185c44e9324
Rodolfo Stoffel Antunes <rodolfo.s.antunes@gmail.com>
2016-06-26 11:42:03 -0700

    Code Documentation
    This commit introduces the documentation of the code implemented in the "logout.php" file.

--- logout.php
+++ logout.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * This is a simple PHP script that handles the procedures to finalize
+ * a session from the user in the system. After destroying the user
+ * session information, this script redirects the ser to the "login.php" script.
+ */
 session_start();
 if (!isset($_SESSION['auth']) || $_SESSION['auth'] != 1) {
 	header('Location: login.php');
