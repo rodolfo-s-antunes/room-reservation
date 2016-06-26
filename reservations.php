a54f4f364b7eb38b49275d3e4742c298ba6906b9
Rodolfo Stoffel Antunes <rodolfo.s.antunes@gmail.com>
2016-06-26 11:46:46 -0700

    Code Documentation
    This commit introduces the documentation of the code implemented in the "reservations.php" file.

--- reservations.php
+++ reservations.php
@@ -1,9 +1,20 @@
 <?php
+// First start the user session and verify if the user is authenticated.
+// If not, redirect to the authentication interface on "login.php".
 session_start();
 if (!isset($_SESSION['auth']) || $_SESSION['auth'] != 1) {
 	header('Location: login.php');
 	exit();
 }
+
+/*
+ * This script is simply an entry point to the "list-reservations.php" interface.
+ * It presents a form that allows the user to input the date for which reservations
+ * should be presented. This form is powered by the jQuery datepicker type to
+ * allow higher browser compatibility. Also, when the form is loaded, it is 
+ * preset with the current date.
+ */
+
 ?>
 
 <script>
